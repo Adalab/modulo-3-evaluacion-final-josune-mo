a58bf867aca36b4b63db52bf493f88717516c124
josune-mo <josune.mo@gmail.com>
2025-06-30 18:27:58 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/modulo-3-evaluacion-final-josune-mo/',
   server: {
     open: "/index.html",
     watch: {
